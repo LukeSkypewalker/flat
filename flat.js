83a92089dce79f1e6e20367f556284b43ad9b543
LukeSkypewalker <work37@gmail.com>
2015-10-15 05:21:38 +0300

    v0.3 type and price are working +cleaning

--- flat.js
+++ flat.js
@@ -1,31 +1,48 @@
+//TODO
+//list of stations 
+//+ with coordinates
+//MAP, coordinates, MongoDB GEO indexes
+
+
+
 Queries = new Mongo.Collection("queries");
-src="";
-dst="";
-//mtchs=[];
 
 if (Meteor.isClient) {
 
-  Template.q.events({
+  Template.qw.events({
     'submit form': function (e) {
       e.preventDefault();
   
-      src=$(e.target).find('[name=source]').val();
-      dst=$(e.target).find('[name=dest]').val();
-      // Insert a query into the collection
+      src=$(e.target).find('[name=src]').val();
+      dst=$(e.target).find('[name=dst]').val();
+      srcType=$(e.target).find('[name=srcType]').val();
+      dstType=$(e.target).find('[name=dstType]').val();
+      srcPrice=$(e.target).find('[name=srcPrice]').val();
+      dstPrice=$(e.target).find('[name=dstPrice]').val();
+      email=$(e.target).find('[name=email]').val();
+
       Queries.insert({
-        source: src,
-        dest: dst
+        src: src,
+        dst: dst,
+        srcType: srcType,
+        dstType: dstType,
+        srcPrice: srcPrice,
+        dstPrice: dstPrice,
+        email: email
       });
  
-      Session.set('mtchs', Queries.find({
-         source: $(e.target).find('[name=dest]').val(), 
-         dest: $(e.target).find('[name=source]').val() 
-      }).fetch() );
+      Session.set('matches', 
+        Queries.find({
+         src: dst, 
+         dst: src,
+        })
+      .fetch());
 
-      console.log(Session.get('mtchs'))
-      //Clear form
-      //$(e.target).find('[name=source]').val("");
-      //$(e.target).find('[name=dest]').val("");
+      console.log(Session.get('matches'))
+      
+      //if you want Clear form
+      //$(e.target).find('[name=src]').val("");
+      //$(e.target).find('[name=dst]').val("");
     }
   });
 
@@ -37,24 +54,14 @@ if (Meteor.isClient) {
 
   Template.body.helpers({
     matches: function () {
-      //TODO VARIABLES - Queries.find({source: XXX, dest: YYY });
       //TODO HIDE and show after submit
-      //console.log(src);
-      return Session.get('mtchs');
+      return Session.get('matches');
     },
 
     allDataBase: function () {
       return Queries.find({});
     }
   });
-
-  Template.body.events({
-    "submit.new-query": function (event) {
-      // Prevent default browser form submit
-      event.preventDefault();
-    }
-  });
-
 
 }
 
